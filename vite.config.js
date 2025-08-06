b4911fccef165809d4857988147e04fad7a4507b
MiguelNewtral <miguel.ros@newtral.es>
2025-08-06 17:15:10 +0200

    “subida”

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
-
-// https://vite.dev/config/
+import { defineConfig } from ‘vite’;
+import { svelte } from ‘@sveltejs/vite-plugin-svelte’;
 export default defineConfig({
-  plugins: [svelte()],
-})
+	plugins: [svelte()],
+	base: ‘/gaza_ayuda/‘,
+barras
+});
